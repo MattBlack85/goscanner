f57568880a594441894e0896c869d80f08b75470
Mattia Procopio <promat85@gmail.com>
2017-01-26 11:08:25 +0100

    First basic version

---
+++ scanner.go
@@ -0,0 +1,61 @@
+package main
+
+import "bufio"
+import "fmt"
+import "io"
+import "log"
+import "os"
+import "strings"
+import "sync"
+import "net/http"
+
+
+var client = &http.Client{}
+
+
+func check_url(full_url string, portion string, wg *sync.WaitGroup) {
+	resp, err := client.Get(full_url)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Printf("Got an error: %s", err)
+	}
+		
+	fmt.Printf("Scanning /%s: %d\n", portion, resp.StatusCode)
+	wg.Done()
+	return
+}
+
+func main() {
+	var waitGroup sync.WaitGroup
+	
+	args := os.Args
+	path := args[1]
+	base_url := args[2]
+	
+	file, err := os.Open(path)
+	
+	defer file.Close()
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	reader := bufio.NewReader(file)
+	
+	for {
+		str, err := reader.ReadString(10)
+		trimmed_str := strings.TrimSuffix(str, "\n")
+		
+		if (err == io.EOF) {
+			break
+		} else if err != nil {
+			fmt.Printf("Error: %q\n", err)
+		}
+		
+		full_url := fmt.Sprintf("%s/%s", base_url, trimmed_str)
+		waitGroup.Add(1)
+		go check_url(full_url, trimmed_str, &waitGroup)
+	}
+
+	waitGroup.Wait()
+}
