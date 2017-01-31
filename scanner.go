a9fbd044810c1d4069bbec455e721bde95e348ee
Mattia Procopio <promat85@gmail.com>
2017-01-31 13:32:04 +0100

    Use Sync for results

--- scanner.go
+++ scanner.go
@@ -6,37 +6,37 @@ import "io"
 import "log"
 import "os"
 import "strings"
+import "sync"
 import "net/http"
-
 
 var client = &http.Client{}
 
-
-func worker(jobs <-chan string, results chan <- string) {
+func worker(jobs <-chan string, wg *sync.WaitGroup) {
 	for url := range jobs {
 		resp, err := client.Get(url)
+
 		if err != nil {
-			results <- fmt.Sprintf("Got an error: %s\n", err)
+			fmt.Printf("Got an error: %s\n", err)
+			wg.Done()
 			continue
 		}
-
 		if resp.StatusCode == 200 {
-			results <- fmt.Sprintf("/%s: Found\n", url)
-		} else {
-			results <- fmt.Sprintf("&%s: Not found\n", url)
+			fmt.Printf("%s: Found\n", url)
 		}
 		resp.Body.Close()
+		wg.Done()
 	}
 }
 
-
 func main() {
+	var waitGroup sync.WaitGroup
+
 	args := os.Args
 	path := args[1]
 	base_url := args[2]
-	
+
 	file, err := os.Open(path)
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,30 +44,28 @@ func main() {
 	defer file.Close()
 	reader := bufio.NewReader(file)
 
-	jobs := make(chan string)
-	results := make(chan string)
+	jobs := make(chan string, 4613)
 
-	for w := 1; w <= 50; w++ {
-		go worker(jobs, results)
+	for w := 1; w <= 250; w++ {
+		go worker(jobs, &waitGroup)
 	}
-	
+
 	for {
 		str, err := reader.ReadString(10)
 		trimmed_str := strings.TrimSuffix(str, "\n")
-		
-		if (err == io.EOF) {
+
+		if err == io.EOF {
 			break
 		} else if err != nil {
 			fmt.Printf("Error: %q\n", err)
 		}
-		
+		waitGroup.Add(1)
 		full_url := fmt.Sprintf("%s/%s", base_url, trimmed_str)
 		jobs <- full_url
 	}
 
 	close(jobs)
 
-	for range results {
-		<- results
-	}
+	waitGroup.Wait()
+
 }
