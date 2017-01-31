ba026b9c0a9eaabb90c44597ca36d4a577df4276
Mattia Procopio <promat85@gmail.com>
2017-01-31 11:06:45 +0100

    Fetch urls with workers

--- scanner.go
+++ scanner.go
@@ -6,45 +6,51 @@ import "io"
 import "log"
 import "os"
 import "strings"
-import "sync"
 import "net/http"
 
 
 var client = &http.Client{}
 
 
-func check_url(full_url string, portion string, wg *sync.WaitGroup) {
-	resp, err := client.Get(full_url)
+func worker(jobs <-chan string, results chan <- string) {
 
-	if err != nil {
-		fmt.Printf("Got an error: %s\n", err)
-		return
-	}
+	for url := range jobs {
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Printf("Got an error: %s\n", err)
+			return
+		}
 
-	if resp.StatusCode == 200 {
-		fmt.Printf("/%s: Found\n", portion)
+		if resp.StatusCode == 200 {
+			results <- fmt.Sprintf("/%s: Found\n", url)
+		} else {
+			results <- fmt.Sprintf("&%s: Not found\n", url)
+		}
+		resp.Body.Close()
 	}
-	resp.Body.Close()
-	wg.Done()
-	return
 }
 
+
 func main() {
-	var waitGroup sync.WaitGroup
-	
 	args := os.Args
 	path := args[1]
 	base_url := args[2]
 	
 	file, err := os.Open(path)
 	
-	defer file.Close()
-	
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
+	defer file.Close()
 	reader := bufio.NewReader(file)
+
+	jobs := make(chan string)
+	results := make(chan string)
+
+	for w := 1; w <= 50; w++ {
+		go worker(jobs, results)
+	}
 	
 	for {
 		str, err := reader.ReadString(10)
@@ -57,9 +63,12 @@ func main() {
 		}
 		
 		full_url := fmt.Sprintf("%s/%s", base_url, trimmed_str)
-		waitGroup.Add(1)
-		go check_url(full_url, trimmed_str, &waitGroup)
+		jobs <- full_url
 	}
 
-	waitGroup.Wait()
+	close(jobs)
+
+	for range results {
+		<- results
+	}
 }
